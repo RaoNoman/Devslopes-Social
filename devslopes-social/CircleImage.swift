de8cb1cefed7e0b1301ea0d9f674dbd5dd203bfc
Rao Noman <raonoman@Raos-MacBook-Pro.local>
2017-11-01 09:18:50 +0500

    feedcell complete

---
+++ devslopes-social/CircleImage.swift
@@ -0,0 +1,21 @@
+//
+//  CircleImage.swift
+//  devslopes-social
+//
+//  Created by Rao Noman on 10/31/17.
+//  Copyright © 2017 Rao Noman. All rights reserved.
+//
+
+import UIKit
+
+class CircleImage: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
